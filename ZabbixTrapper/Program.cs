82dfdea873e08db94d70c703057967b383a3904f
jaandk <jaandk@gmail.com>
2018-07-07 20:20:15 +0200

    first commit

--- ZabbixTrapper/Program.cs
+++ ZabbixTrapper/Program.cs
@@ -1,7 +1,11 @@
-﻿using Predes.ZabbixSender.Core;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Predes.ZabbixSender.Core;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using uPLibrary.Networking.M2Mqtt;
@@ -11,19 +15,22 @@ namespace ZabbixTrapper
 {
 	class Program
 	{
+		private static Setting setting;
+
 		static void Main(string[] args)
 		{
+			LoadSettingsFile();
 
 			while (!CheckForInternetConnection())
 			{
 				Console.WriteLine($"[{DateTime.Now}] No internet, sleeping");
 				Thread.Sleep(3000);
 			}
-			 
+
 			Console.WriteLine($"[{DateTime.Now}] There is now internet");
 
 			// create client instance 
-			MqttClient client = new MqttClient("10.0.3.7");
+			MqttClient client = new MqttClient(setting.MqttserverIp);
 			// register to message received 
 			client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
@@ -31,12 +38,56 @@ namespace ZabbixTrapper
 			client.Connect(clientId);
 
 			// subscribe to the topic "/home/temperature" with QoS 2 
-			client.Subscribe(new string[] { "bruh/sensornode1" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+			client.Subscribe(setting.MqttProperties.Select(s => s.MqttTopic).ToArray(), new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+
 			Console.WriteLine($"[{DateTime.Now}] Is now subscribed to mqtt");
 			while (true)
 			{
-				Thread.Sleep(3000);
 			}
+		}
+
+		private static void LoadSettingsFile()
+		{
+			string settingsFile = "/config/setting.json";
+			if (File.Exists(settingsFile))
+			{
+				setting = JsonConvert.DeserializeObject<Setting>(File.ReadAllText(settingsFile));
+			}
+			else
+			{
+				var tmpSetting = new Setting();
+				tmpSetting.MqttserverIp = "1.2.3.4";
+				tmpSetting.ZabbixIp = "5.6.7.8";
+				tmpSetting.MqttProperties = new List<MqttProp>();
+				tmpSetting.MqttProperties.Add(new MqttProp() { MqttTopic = "topic1/topic2/topic3", MqttProperties = new List<string>() { "prop1", "prop2" });
+				Directory.CreateDirectory("/config");
+				File.WriteAllText(settingsFile, JsonConvert.SerializeObject(tmpSetting));
+			}
+		}
+
+		private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+		{
+			var resultString = System.Text.Encoding.Default.GetString(e.Message);
+			var sensorData = JObject.Parse(resultString);
+
+			var dataList = new List<SenderData>();
+
+			SenderService service = new SenderService(setting.ZabbixIp, 10051);
+
+			var tt = setting.MqttProperties.FirstOrDefault(s => s.MqttTopic == e.Topic);
+			foreach (var item in tt.MqttProperties)
+			{
+				dataList.Add(new SenderData { Host = TopicFormat(tt.MqttTopic), Key = item, Value = (string)sensorData[item] });
+			}
+
+			//Trace.WriteLine($"Sendeing data temp {sensorData.temperature} and humid {sensorData.humidity}");
+			var response = service.Send(dataList.ToArray());
+			Console.WriteLine($"[{DateTime.Now}] {response.Response}");
+		}
+
+		private static string TopicFormat(string str)
+		{
+			return str.Replace("/", ".");
 		}
 
 		public static bool CheckForInternetConnection()
@@ -55,21 +106,6 @@ namespace ZabbixTrapper
 			{
 				return false;
 			}
-		}
-
-		private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
-		{
-			var resultString = System.Text.Encoding.Default.GetString(e.Message);
-			var sensorData = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(resultString, new { temperature = "", humidity = "" });
-
-			var dataList = new List<SenderData>();
-
-			SenderService service = new SenderService("10.0.3.6", 10051);
-			dataList.Add(new SenderData { Host = "testhost1", Key = "temperature", Value = sensorData.temperature });
-			dataList.Add(new SenderData { Host = "testhost1", Key = "humidity", Value = sensorData.humidity });
-			Trace.WriteLine($"Sendeing data temp {sensorData.temperature} and humid {sensorData.humidity}");
-			var response = service.Send(dataList.ToArray());
-			Console.WriteLine($"[{DateTime.Now}] {response.Response}");
 		}
 	}
 }
---
+++ ZabbixTrapper/Setting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZabbixTrapper
+{
+	public class Setting
+	{
+		public string MqttserverIp { get; set; }
+		public string ZabbixIp { get; set; }
+		public List<MqttProp> MqttProperties { get; set; }
+
+	}
+
+	public class MqttProp
+	{
+		public string MqttTopic { get; set; }
+		public List<string> MqttProperties { get; set; }
+
+	}
+}
